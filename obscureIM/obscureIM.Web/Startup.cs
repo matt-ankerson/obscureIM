23e766ff51430a995f6d52b4b055556cbdc51102
Matt Ankerson <MattA@intergen.org.nz>
2016-03-08 10:13:13 +1300

    base project for signalR backend

---
+++ obscureIM/obscureIM.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+[assembly: OwinStartup(typeof(obscureIM.Web.Startup))]
+
+namespace obscureIM.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
